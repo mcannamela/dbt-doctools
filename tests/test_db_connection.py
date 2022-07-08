1fa8c348cf9dd4b2b840f598ad3409446dc242aa
Michael Cannamela <m.cannamela@gmail.com>
2022-07-08 14:38:13 -0400

    dockerignore

--- tests/test_db_connection.py
+++ tests/test_db_connection.py
@@ -4,6 +4,7 @@ import psycopg2
 def test_db_connection():
     conn = psycopg2.connect(
         host="db",
-        database="dbt_doctools",
+
         user="postgres",
         password="postgres")
+

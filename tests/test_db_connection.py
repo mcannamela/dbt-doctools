45da0084b9a82a47658f4a6f6611490624ffdacd
Michael Cannamela <m.cannamela@gmail.com>
2022-07-10 22:26:33 -0400

    docstring

--- tests/test_db_connection.py
+++ tests/test_db_connection.py
@@ -2,6 +2,7 @@ from test_support.db_fixtures import db_cursor
 
 
 def test_db_connection(db_cursor):
+    """Just show that we can talk to the database at all."""
     db_cursor.execute("SELECT * FROM information_schema.tables")
     tables = db_cursor.fetchall()
 

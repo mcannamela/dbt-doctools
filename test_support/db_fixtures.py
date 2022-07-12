06e10704b12828dd6c5952c7b98b6ee6dcc6a3ec
Michael Cannamela <m.cannamela@gmail.com>
2022-07-11 21:28:28 -0400

    now with tenacity

--- test_support/db_fixtures.py
+++ test_support/db_fixtures.py
@@ -3,15 +3,22 @@ import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from psycopg2.errors import DuplicateDatabase
 from pytest import fixture
+from tenacity import retry, wait_exponential, retry_if_exception_type, retry_if_exception_message
+
+@retry(retry=retry_if_exception_type(psycopg2.OperationalError) &
+             retry_if_exception_message(match='Connection refused'),
+       wait=wait_exponential(multiplier=1, min=4, max=10))
+def connect_to_test_db():
+    psycopg2.connect(
+        host="db",
+        database='dbt_doctools_test',
+        user="postgres",
+        password="postgres")
 
 @fixture(scope='module')
 def db_conn():
     try:
-        yield psycopg2.connect(
-            host="db",
-            database='dbt_doctools_test',
-            user="postgres",
-            password="postgres")
+        conn_with_db = connect_to_test_db()
     except psycopg2.OperationalError as exc:
         if 'database "dbt_doctools_test" does not exist' in str(exc):
             conn = psycopg2.connect(
@@ -26,15 +33,12 @@ def db_conn():
                 except DuplicateDatabase:
                     pass
 
-            conn_with_db = psycopg2.connect(
-                host="db",
-                database='dbt_doctools_test',
-                user="postgres",
-                password="postgres")
-
-            yield conn_with_db
+            conn_with_db = connect_to_test_db()
         else:
             raise
+
+    yield conn_with_db
+
 
 @fixture
 def db_cursor(db_conn):

e5c85cbb7d1c284c02bb21ee915d4e0ae4d8073e
Michael Cannamela <m.cannamela@gmail.com>
2022-08-02 22:58:55 -0400

    sketch some operations on sources

--- dbt_doctools/source_ops.py
+++ dbt_doctools/source_ops.py
@@ -1,9 +1,11 @@
-from typing import Tuple
+from typing import Tuple, Dict, Any, TypeVar, Iterable, Callable, Optional
 
 from dbt.config import RuntimeConfig
 from dbt.contracts.files import SchemaSourceFile
 from dbt.contracts.graph.manifest import Manifest
 from dbt.contracts.graph.parsed import ParsedSourceDefinition
+
+from dbt_doctools.yaml_ops import unsafe_get_matching_singleton_by_key, YamlFragment
 
 
 def blah():
@@ -17,15 +19,37 @@ def blah():
     assert set(column_descriptions.keys()) == {'old_source_id', 'value'}
 
 
-def extract_source(m: Manifest, c: RuntimeConfig, source_name: str, table_name: str) -> Tuple[
+def extract_source(m: Manifest, project_name, source_name: str, table_name: str) -> Tuple[
     ParsedSourceDefinition, SchemaSourceFile]:
     has_sources = {k: f for k, f in m.files.items() if isinstance(f, SchemaSourceFile) and f.sources}
-    maybe_s = m.resolve_source(source_name, table_name, c.project_name, None)
+    maybe_s = m.resolve_source(source_name, table_name, project_name, None)
     if maybe_s is not None:
         f = has_sources[maybe_s.file_id]
         return maybe_s, f
     else:
         raise LookupError(
-            f"Nothing found for source '{source_name}' table '{table_name}' in project '{c.project_name}'")
+            f"Nothing found for source '{source_name}' table '{table_name}' in project '{project_name}'")
 
 
+def extract_source_yaml_fragment_from_file(file_of_source: SchemaSourceFile, source_name: str) -> Any:
+    source_dfy = unsafe_get_matching_singleton_by_key(
+        ((s['name'], s) for s in file_of_source.dict_from_yaml['sources']),
+        source_name
+    )[1]
+    return source_dfy
+
+
+def refactor_to_doc_blocks(source: ParsedSourceDefinition, file_of_source: SchemaSourceFile):
+    source_dfy = extract_source_yaml_fragment_from_file(file_of_source, source.source_name)
+
+    return source, file_of_source
+
+
+def refactor_to_one_file_per_table(file_of_source: SchemaSourceFile, source_name: str) -> Iterable[YamlFragment]:
+    source_dfy = extract_source_yaml_fragment_from_file(file_of_source, source_name)
+    source_header = {k: v for k, v in source_dfy.items() if k != 'tables'}
+    for table_yaml in source_dfy['tables']:
+        yield dict(
+            version=file_of_source.dict_from_yaml['version'],
+            sources=[dict(**source_header, **dict(tables=table_yaml))]
+        )
--- tests/test_source_ops.py
+++ tests/test_source_ops.py
@@ -1,10 +1,20 @@
 from dbt.contracts.files import SchemaSourceFile
 from dbt.contracts.graph.parsed import ParsedSourceDefinition
 
-from dbt_doctools.source_ops import extract_source
+from dbt_doctools.source_ops import extract_source, refactor_to_doc_blocks, refactor_to_one_file_per_table
 
 
 def test_extract_source(manifest, config):
-    s, f = extract_source(manifest, config, 'sources', 'some_old_source')
+    s, f = extract_source(manifest, config.project_name, 'dummy_sources', 'some_old_source')
     assert isinstance(s, ParsedSourceDefinition)
     assert isinstance(f, SchemaSourceFile)
+
+
+def test_refactor_to_doc_blocks(manifest, config):
+    s, f = extract_source(manifest, config.project_name, 'dummy_sources', 'some_old_source')
+    thing = refactor_to_doc_blocks(s, f)
+
+
+def test_refactor_to_one_file_per_table(manifest, config):
+    s, f = extract_source(manifest, config.project_name, 'dummy_sources', 'some_old_source')
+    thing = [source_yaml for source_yaml in refactor_to_one_file_per_table(f, 'dummy_sources')]

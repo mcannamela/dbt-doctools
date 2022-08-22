704a7eb7e86a0a2820eba5af5d62ae0757efaa96
Michael Cannamela <m.cannamela@gmail.com>
2022-08-22 19:55:33 -0400

    yml and yaml

--- dbt_doctools/yaml_io.py
+++ dbt_doctools/yaml_io.py
@@ -13,7 +13,7 @@ def overwrite_yaml_file(yaml_doc:YamlFragment, schema_file:SchemaSourceFile):
 
 
 def create_or_append_companion_markdown(content:str, schema_file:SchemaSourceFile):
-    path = schema_file.path.full_path.replace('.yaml', '.md')
+    path = schema_file.path.full_path.replace('.yml', '.md').replace('.yaml', '.md')
     logger.info(f"Writing companion markdown to {path}")
     with open(path, 'wa') as f:
         f.write(content)

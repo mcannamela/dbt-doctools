78858102ea2bf2e4665213accaf408ef1d65fca2
Michael Cannamela <m.cannamela@gmail.com>
2022-08-30 14:52:08 -0400

    find the files we need to rewrite to eliminate the duplicate blocks

--- dbt_doctools/docs_ops.py
+++ dbt_doctools/docs_ops.py
@@ -1,7 +1,7 @@
 import functools
 import itertools
 import operator
-from typing import Iterable, List, Dict, Tuple
+from typing import Iterable, List, Dict, Tuple, Callable
 
 from dbt.config import read_user_config, RuntimeConfig
 from dbt.contracts.files import SchemaSourceFile
@@ -19,31 +19,53 @@ def is_non_empty(doc:ParsedDocumentation):
 
 
 def consolidate_duplicate_docs_blocks_(manifest:Manifest, graph:Graph, config:RuntimeConfig):
-    g:DiGraph = graph.graph
+    sort_key = make_doc_sort_fun(manifest, graph, config)
 
+    g:DiGraph = graph.graph
     non_empty_docs = {k:d for k,d in manifest.docs.items() if is_non_empty(d)}
     block_content_to_docs = defaultdict(list)
     for d in non_empty_docs.values():
         block_content_to_docs[d.block_contents.strip()].append(d)
 
-    duplicate_doc_lists = [v for v in block_content_to_docs.values() if len(v) > 1]
+    duplicate_docs_to_remove = sum(
+        [list(sorted(v, key=lambda d: sort_key(d.unique_id)))[1:] for v in block_content_to_docs.values() if len(v) > 1]
+        ,[]
+    )
 
-    duplicate_doc_names = functools.reduce(operator.or_, [{d.name for d in dups} for dups in duplicate_doc_lists ], set())
+    files_to_rewrite = {d.file_id for d in duplicate_docs_to_remove}
 
-    block_to_refs, ref_to_blocks = build_docs_block_to_ref_map(manifest, config.project_name)
-    block_to_sources, source_to_blocks = build_docs_block_to_source_map(manifest, config.project_name)
+    return duplicate_docs_to_remove
 
+
+
+def make_doc_sort_fun(manifest:Manifest, graph:Graph, config:RuntimeConfig):
+    _, ref_to_docs = build_docs_block_to_ref_map(manifest, config.project_name)
+    _, source_to_docs = build_docs_block_to_source_map(manifest, config.project_name)
+
+    def get_blocks_iter(n):
+        return itertools.chain(source_to_docs.get(n, []), ref_to_docs.get(n, []))
+
+    doc_depth = compute_doc_depth(graph.graph, get_blocks_iter)
+
+    def sort_key(doc_name):
+        return (doc_depth.get(doc_name, 2 ** 31), doc_name)
+
+    return sort_key
+
+
+def compute_doc_depth(g, get_blocks_iter:Callable[[str], Iterable[str]]):
     doc_depth = {}
 
-    node_set = {n for n in g if g.in_degree(n)==0}
+
+
+    node_set = {n for n in g if g.in_degree(n) == 0}
     depth = 0
     while node_set:
         for n in node_set:
-            for b in itertools.chain(source_to_blocks.get(n, []), ref_to_blocks.get(n, [])):
+            for b in get_blocks_iter(n):
                 doc_depth[b] = depth
         node_set = set(itertools.chain(*[g.successors(n) for n in node_set]))
         depth += 1
-
     return doc_depth
 
 

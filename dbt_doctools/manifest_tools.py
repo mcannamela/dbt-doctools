2f610102f5325045896f3cce50c8fc8002f5848f
Michael Cannamela <m.cannamela@gmail.com>
2022-09-28 18:00:51 -0400

    Common tools to work with the manifest

---
+++ dbt_doctools/manifest_tools.py
@@ -0,0 +1,69 @@
+import itertools
+from collections import defaultdict
+from typing import Iterable, Dict, Tuple, Callable, Set
+
+from dbt.contracts.files import SchemaSourceFile
+from dbt.contracts.graph.manifest import Manifest
+
+from dbt_doctools.markdown_ops import DocsBlock
+from dbt_doctools.yaml_ops import YamlMap, YamlList
+
+IdSetMap = Dict[str, Set[str]]
+
+
+def build_docs_block_to_ref_map(
+        extract_ref_id_and_columns: Callable[[YamlMap], Tuple[str, YamlList]],
+        extract_source_id_and_columns: Callable[[Tuple[str, YamlMap]], Tuple[str, YamlList]],
+        node_or_sources_files_iter: Iterable[SchemaSourceFile]
+) -> Tuple[IdSetMap, IdSetMap, IdSetMap]:
+    block_id_to_model_ids = defaultdict(set)
+    model_id_to_block_ids = defaultdict(set)
+    block_id_to_file_ids = defaultdict(set)
+
+    for fid, f in node_or_sources_files_iter:
+        ref_or_source_iter = itertools.chain(
+            zip(f.dict_from_yaml.get('models', []), itertools.repeat(extract_ref_id_and_columns)),
+            zip(f.dict_from_yaml.get('seeds', []), itertools.repeat(extract_ref_id_and_columns)),
+            zip(iter_source_tables(f), itertools.repeat(extract_source_id_and_columns)),
+        )
+        for dfy, extract_fun in ref_or_source_iter:
+            ref_or_source_id, columns_iter = extract_fun(dfy)
+            for column_dfy in columns_iter:
+                referenced_block_names = DocsBlock.referenced_doc_names(column_dfy.get('description', ''))
+                for b in referenced_block_names:
+                    block_id_to_model_ids[b].add(ref_or_source_id)
+                    model_id_to_block_ids[ref_or_source_id].add(b)
+                    block_id_to_file_ids[b].add(fid)
+
+    return block_id_to_model_ids, model_id_to_block_ids, block_id_to_file_ids
+
+
+def ref_id_and_column_extractor(manifest:Manifest, project_name:str)->Callable[[YamlMap], Tuple[str, YamlList]]:
+    def extract_ref_id_and_columns(y:YamlMap)->Tuple[str, YamlList]:
+        return manifest.ref_lookup.get_unique_id(y['name'], project_name), y.get('columns',[])
+
+    return extract_ref_id_and_columns
+
+
+def source_id_and_column_extractor(manifest:Manifest, project_name:str)->Callable[[Tuple[str, YamlMap]], Tuple[str, YamlList]]:
+    def extract_source_id_and_columns(t: Tuple[str, YamlMap]) -> Tuple[str, YamlList]:
+        source_name = t[0]
+        y = t[1]
+        return manifest.resolve_source(source_name, y['name'], project_name, None).unique_id, y.get('columns', [])
+
+    return extract_source_id_and_columns
+
+
+def iter_schema_source_files(manifest:Manifest)->Iterable[SchemaSourceFile]:
+    for fid, f in manifest.files.items():
+        if isinstance(f, SchemaSourceFile):
+            yield fid, f
+
+
+def iter_node_or_sources_files(manifest:Manifest)->Iterable[SchemaSourceFile]:
+    for fid, f in iter_schema_source_files(manifest):
+        if f.node_patches or f.sources:
+            yield fid, f
+
+def iter_source_tables(f: SchemaSourceFile)->Iterable[Tuple[str, YamlMap]]:
+    return itertools.chain(*(((s['name'], t) for t in s.get('tables', [])) for s in f.dict_from_yaml.get('sources', [])))
---
+++ tests/test_manifest_tools.py
@@ -0,0 +1,106 @@
+from dbt.contracts.files import SchemaSourceFile
+
+from dbt_doctools.manifest_tools import build_docs_block_to_ref_map, iter_node_or_sources_files, \
+    iter_schema_source_files, source_id_and_column_extractor, ref_id_and_column_extractor, iter_source_tables
+
+
+def test_build_docs_block_to_ref_map(manifest, config):
+    project_name = config.project_name
+    block_id_to_model_ids, model_id_to_block_ids, block_id_to_file_ids = build_docs_block_to_ref_map(
+        ref_id_and_column_extractor(manifest, project_name),
+        source_id_and_column_extractor(manifest, project_name),
+        iter_node_or_sources_files(manifest)
+    )
+
+    assert block_id_to_model_ids == {
+        'static_map_value': {'seed.dummy.some_static_map'},
+        'zero_layer__old_value': {'model.dummy.old_zero_layer'},
+        'one_layer__old_value': {'model.dummy.old_one_layer'},
+        'some_old_source__value': {'source.dummy.dummy_sources.some_old_source'}
+    }
+
+    assert model_id_to_block_ids == {
+        'seed.dummy.some_static_map': {'static_map_value'},
+        'model.dummy.old_zero_layer': {'zero_layer__old_value'},
+        'model.dummy.old_one_layer': {'one_layer__old_value'},
+        'source.dummy.dummy_sources.some_old_source': {'some_old_source__value'}
+    }
+
+    assert block_id_to_file_ids == {
+        'static_map_value': {'dummy://seeds/some_static_map.yml'},
+        'zero_layer__old_value': {'dummy://models/zero_layer/schema.yml'},
+        'one_layer__old_value': {'dummy://models/zero_layer/schema.yml'},
+        'some_old_source__value': {'dummy://models/sources/some_sources.yml'}
+    }
+
+
+def test_ref_id_and_column_extractor(manifest, config):
+    project_name = config.project_name
+    extractor = ref_id_and_column_extractor(manifest, project_name)
+    ref_name = 'old_zero_layer'
+    exp_columns = 'fake_columns'
+    ref_id, columns = extractor({'name': ref_name, 'columns': exp_columns})
+    assert columns == exp_columns
+    assert ref_id == f'model.{project_name}.{ref_name}'
+
+
+def test_ref_id_and_column_extractor_ok_when_columns_dne(manifest, config):
+    project_name = config.project_name
+    extractor = ref_id_and_column_extractor(manifest, project_name)
+    ref_name = 'old_zero_layer'
+    ref_id, columns = extractor({'name': ref_name})
+    assert columns == []
+    assert ref_id == f'model.{project_name}.{ref_name}'
+
+
+def test_source_id_and_column_extractor(manifest, config):
+    project_name = config.project_name
+    extractor = source_id_and_column_extractor(manifest, project_name)
+
+    source_name = 'dummy_sources'
+    table_name = 'some_old_source'
+    exp_columns = 'fake_columns'
+    ref_id, columns = extractor((source_name, {'name': table_name, 'columns': exp_columns}))
+    assert columns == exp_columns
+    assert ref_id == f'source.{project_name}.{source_name}.{table_name}'
+
+
+def test_source_id_and_column_extractor_ok_when_columns_dne(manifest, config):
+    project_name = config.project_name
+    extractor = source_id_and_column_extractor(manifest, project_name)
+
+    source_name = 'dummy_sources'
+    table_name = 'some_old_source'
+    ref_id, columns = extractor((source_name, {'name': table_name}))
+    assert columns == []
+    assert ref_id == f'source.{project_name}.{source_name}.{table_name}'
+
+
+def test_iter_schema_source_files(manifest):
+    files_iter = iter_schema_source_files(manifest)
+    for fid, f in files_iter:
+        assert isinstance(f, SchemaSourceFile)
+        assert manifest.files[fid] == f
+
+
+def test_iter_node_or_sources_files(manifest):
+    files_iter = iter_node_or_sources_files(manifest)
+    for fid, f in files_iter:
+        assert f.node_patches or f.sources
+        assert manifest.files[fid] == f
+
+
+def test_iter_node_or_sources_files_subset_of_iter_schema_source_files(manifest):
+    schema_fids = {fid for fid, _ in iter_schema_source_files(manifest)}
+    nodes_fids = {fid for fid, _ in iter_node_or_sources_files(manifest)}
+    assert nodes_fids <= schema_fids
+
+
+def test_iter_source_tables(manifest, config):
+    source_tables = list(iter_source_tables(manifest.files['dummy://models/sources/some_sources.yml']))
+    assert {(source_name, table['name']) for source_name, table in source_tables} == {('dummy_sources', 'some_old_source'), ('dummy_sources', 'some_new_source'), }
+    for _, table in source_tables:
+        assert 'columns' in table
+
+
+

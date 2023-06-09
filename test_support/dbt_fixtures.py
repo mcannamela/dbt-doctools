05dd48f7fad895be142dd0e95923d117f61c7bd1
michael <m.cannamela@gmail.com>
2023-06-09 15:35:11 -0400

    does in fact rollback if we use sh

--- test_support/dbt_fixtures.py
+++ test_support/dbt_fixtures.py
@@ -3,37 +3,23 @@ from pytest import fixture
 
 from dbt_doctools.dbt_api import obtain_manifest_and_graph_and_config
 from git import Repo
-
+import sh
 def repo_top_level():
     return Path(__file__).parent.parent
 
 def dbt_dummy_project_path():
     return repo_top_level() / 'dummy_dbt_project' / 'dummy'
 
-def dbt_dummy_template():
-    return repo_top_level() / 'dummy_dbt_project' / 'dummy'
+@fixture()
+def dummy_project_files():
+    sh.git(*(f"config --global --add safe.directory {repo_top_level()}".split()))
+    sh.git(*(f"restore --source=HEAD --staged --worktree -- {dbt_dummy_project_path()}".split()))
+    yield
+    sh.git(*(f"restore --source=HEAD --staged --worktree -- {dbt_dummy_project_path()}".split()))
 
 
 @fixture()
-def markdown_files():
-    markdowns = [p.relative_to(repo_top_level()) for p in dbt_dummy_project_path().rglob('*.md')]
-    yield markdowns
-    reset_files_to_index(markdowns)
-
-@fixture()
-def yaml_files():
-    yamls = dbt_dummy_project_path().rglob('*.yml')
-    yield yamls
-    reset_files_to_index(yamls)
-
-
-def reset_files_to_index(yamls):
-    repo = Repo(repo_top_level())
-    repo.index.checkout(yamls, force=True)
-
-
-@fixture()
-def manifest_and_graph_and_config():
+def manifest_and_graph_and_config(dummy_project_files):
     m, g, c = obtain_manifest_and_graph_and_config(
         dbt_command_args=['--project-dir', str(dbt_dummy_project_path())]
     )

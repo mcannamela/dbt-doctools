ab0af48ad0eb6b591370f2e7de67b8042c612b06
michael <m.cannamela@gmail.com>
2023-06-08 22:39:02 -0400

    try and fixture markdown and yaml files

--- test_support/dbt_fixtures.py
+++ test_support/dbt_fixtures.py
@@ -2,12 +2,40 @@ from pathlib import Path
 from pytest import fixture
 
 from dbt_doctools.dbt_api import obtain_manifest_and_graph_and_config
+from git import Repo
+
+def repo_top_level():
+    return Path(__file__).parent.parent
+
+def dbt_dummy_project_path():
+    return repo_top_level() / 'dummy_dbt_project' / 'dummy'
+
+def dbt_dummy_template():
+    return repo_top_level() / 'dummy_dbt_project' / 'dummy'
+
+
+@fixture()
+def markdown_files():
+    markdowns = [p.relative_to(repo_top_level()) for p in dbt_dummy_project_path().rglob('*.md')]
+    yield markdowns
+    soft_reset(markdowns)
+
+@fixture()
+def yaml_files():
+    yamls = dbt_dummy_project_path().rglob('*.yml')
+    yield yamls
+    soft_reset(yamls)
+
+
+def soft_reset(yamls):
+    repo = Repo(repo_top_level())
+    repo.index.checkout(yamls, force=True)
 
 
 @fixture()
 def manifest_and_graph_and_config():
     m, g, c = obtain_manifest_and_graph_and_config(
-        dbt_command_args=['--project-dir', str(Path(__file__).parent.parent / 'dummy_dbt_project' / 'dummy')]
+        dbt_command_args=['--project-dir', str(dbt_dummy_project_path())]
     )
     return m, g, c
 
@@ -20,6 +48,8 @@ def manifest(manifest_and_graph_and_config):
 @fixture()
 def graph(manifest_and_graph_and_config):
     return manifest_and_graph_and_config[1]
+
+
 
 
 @fixture()

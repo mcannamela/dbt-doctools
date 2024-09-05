fc5affcf8c61901f73e2cf575088bca243866846
michael <m.cannamela@gmail.com>
2024-09-05 11:06:50 -0400

    the dbt api has changed

--- dbt_doctools/dbt_api.py
+++ dbt_doctools/dbt_api.py
@@ -2,22 +2,39 @@ from typing import Tuple, List
 
 import dbt
 from dbt import flags
-from dbt.config import read_user_config, RuntimeConfig
+from dbt.config import RuntimeConfig
 from dbt.contracts.graph.manifest import Manifest
 from dbt.graph import Graph
+from dbt.cli.main
 from dbt.main import parse_args, adapter_management
+from dbt.config.profile import read_user_config
 
+# from dbt.cli import dbt_cli
+#
+# if __name__ == "__main__":
+#     dbt_cli(
+#         [
+#             "build",
+#             "--profiles-dir",
+#             "/opt/project/dummy_dbt_project",
+#             "--project-dir",
+#             "/opt/project/dummy_dbt_project/dummy",
+#         ]
+#     )
 
-def obtain_manifest_and_graph_and_config(dbt_command: str = 'compile', dbt_args: List[str] = tuple(),
-                                         dbt_command_args: List[str] = tuple()) -> Tuple[Manifest, Graph, RuntimeConfig]:
+def obtain_manifest_and_graph_and_config(
+    dbt_command: str = "compile",
+    dbt_args: List[str] = tuple(),
+    dbt_command_args: List[str] = tuple(),
+) -> Tuple[Manifest, Graph, RuntimeConfig]:
     """Get a fully-loaded dbt manifest by hijacking a compile task
 
     Much of this is copy-pasta from dbt's own interals, found by following the main entrypoint.
     """
-    parsed = parse_args(list(dbt_args) + [dbt_command] + list(dbt_command_args))
 
     # Set flags from args, user config, and env vars
-    user_config = read_user_config(flags.PROFILES_DIR)
+    user_config = read_user_config(flags.PROFILES_DIR)  # This is read again later
+    parsed = parse_args(list(dbt_args) + [dbt_command] + list(dbt_command_args))
     flags.set_from_args(parsed, user_config)
     dbt.tracking.initialize_from_flags()
 
